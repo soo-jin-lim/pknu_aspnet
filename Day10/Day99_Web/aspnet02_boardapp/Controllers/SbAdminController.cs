57d907dfc6b3403f8ee692934d810879ea69ffff
soo-jin-lim <tn7969@naver.com>
2023-05-30 15:19:43 +0900

    .

---
+++ Day10/Day99_Web/aspnet02_boardapp/Controllers/SbAdminController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace aspnet02_boardapp.Controllers
+{
+    public class SbAdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
